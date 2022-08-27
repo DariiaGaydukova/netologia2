62527ed9fb69639243674e68d85ab85a107db7de
Dariia Gaydukova <daria1171@gmail.com>
2022-08-27 10:09:15 +0300

    Версия 1

---
+++ netologiaGit2/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        long[] sales1 = {200, 300, 400};
+        SalesManager Ivanov = new SalesManager(sales1);
+        long max1 = Ivanov.max();
+
+
+        long[] sales2 = {1200, 200, 100};
+        SalesManager Petrov = new SalesManager(sales2);
+
+        long average = Petrov.average();
+        long max2 = Petrov.max();
+
+
+    }
+
+}
